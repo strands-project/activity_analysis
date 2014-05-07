11d4f238c2ff2eefe65741325496729ce0d837df
Christian Dondrup <cdondrup@lincoln.ac.uk>
2014-05-07 10:15:37 +0100

    Working and publishing version of engagement checker. Takes advantage of the functionality of a dictionary to remove magic numbers.

--- strands_engagement_checker/src/engage_check.py
+++ strands_engagement_checker/src/engage_check.py
@@ -9,22 +9,26 @@ class EngagementChecker:
     def __init__(self, name='engagement_checker'):
         rospy.init_node(name)
 
-        self.detections = {}
-        for i in range(100): #TODO: Find nicer solution
-            self.detections[i] = 0
+        self.detections = dict()
 
         pub_topic = rospy.get_param("~detections",name+"/engaged")
+        self.threshold = rospy.get_param("~threshold", 20)
+
         self.pub = rospy.Publisher(pub_topic,std_msgs.msg.Bool,queue_size=100)
         rospy.Subscriber('/pedestrian_localisation/localisations', PedestrianLocations, self.callback_pose)
 
     def callback_pose(self, data):
-        if len(data.ids)>0:
-            x = range(100)
-    	for i in list(data.ids):
-    	    x.remove(i)
-    	for i in x:
-    	    self.detections[i] = 0
+        # Add new detections
+        for idx in data.ids:
+            if not idx in self.detections.keys():
+                self.detections[idx] = 0
 
+        # Remove old detections
+        for idx in self.detections.keys():
+            if not idx in data.ids:
+                del(self.detections[idx])
+
+        # Check for engagement
         for i in range(len(data.distances)):
             if data.distances[i]<1.3:
                 self.detections[data.ids[i]] += 1
@@ -32,15 +36,12 @@ class EngagementChecker:
             else:
                 self.detections[data.ids[i]] = 0
 
-            if self.detections[data.ids[i]] == 20:
+            if self.detections[data.ids[i]] == self.threshold:
                 result = std_msgs.msg.Bool()
                 result.data = True
                 self.pub.publish(result)
 
-
 if __name__ == '__main__':
-
-    kin = EngagementChecker()
-    print('Engagement checker running ...')
-    kin.get_posture()
+    ec = EngagementChecker()
+    rospy.loginfo('Engagement checker running ...')
     rospy.spin()
