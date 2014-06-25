4ec6071ed515c652f08d0c2df739431a4bfa06e8
strands <strands@strands-Mi956.(none)>
2014-06-25 15:18:15 +0100

    integrated with face detector

--- strands_engagement_checker/src/engage_check.py
+++ strands_engagement_checker/src/engage_check.py
@@ -4,6 +4,7 @@
 import rospy
 import std_msgs.msg
 from strands_perception_people_msgs.msg import PedestrianLocations
+from strands_perception_people_msgs.msg import HeadOrientations
 
 class EngagementChecker:
     def __init__(self, name='engagement_checker'):
@@ -14,8 +15,9 @@ class EngagementChecker:
         pub_topic = rospy.get_param("~detections",name+"/engaged")
         self.threshold = rospy.get_param("~threshold", 20)
 
-        self.pub = rospy.Publisher(pub_topic,std_msgs.msg.Bool,queue_size=100)
+        self.pub = rospy.Publisher(pub_topic,std_msgs.msg.Bool,queue_size=1000)
         rospy.Subscriber('/pedestrian_localisation/localisations', PedestrianLocations, self.callback_pose)
+	rospy.Subscriber('/head_orientation/head_ori', HeadOrientations, self.callback_ori)
 
     def callback_pose(self, data):
         # Add new detections
@@ -30,16 +32,23 @@ class EngagementChecker:
 
         # Check for engagement
         for i in range(len(data.distances)):
-            if data.distances[i]<1.3:
-                self.detections[data.ids[i]] += 1
-                rospy.logdebug('found person to probably engage with %s %s',data.ids[i],self.detections[data.ids[i]])
-            else:
-                self.detections[data.ids[i]] = 0
 
-            if self.detections[data.ids[i]] == self.threshold:
+            if data.distances[i]<1.3 and self.detections[data.ids[i]] >= self.threshold:
                 result = std_msgs.msg.Bool()
                 result.data = True
                 self.pub.publish(result)
+		print 'engaging with',data.ids[i]
+            elif data.distances[i]<1.3 and self.detections[data.ids[i]] < self.threshold:
+                self.detections[data.ids[i]] += 1
+                rospy.logdebug('found person to probably engage with %s %s',data.ids[i],self.detections[data.ids[i]])
+		print 'found person to probably engage with',data.ids[i],self.detections[data.ids[i]]
+            else:
+                self.detections[data.ids[i]] = 0
+
+
+    def callback_ori(self, data):
+	print data.angles
+
 
 if __name__ == '__main__':
     ec = EngagementChecker()
